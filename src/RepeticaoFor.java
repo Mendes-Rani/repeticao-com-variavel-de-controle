f3309c9a803e789b1c825b9a51f76451a26a254d
Rani <ranieridasilvamendes159@gmail.com>
2026-04-26 14:49:51 -0300

    Adição de Comentários ao código

--- src/RepeticaoFor.java
+++ src/RepeticaoFor.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+/** Exemplo simples de estrutura de repetição for, onde a variável de controle é declarada dentro do próprio comando for.
+ * O comando for é composto por três partes: a inicialização da variável de controle (int cc=0), a condição de continuação (cc<=3) e a atualização da variável de controle (cc++).
+ * O bloco de código dentro do for será executado enquanto a condição for verdadeira, ou seja, enquanto cc for menor ou igual a 3. A cada iteração, o valor de cc será incrementado em 1, e a mensagem "Cambalhota!" será impressa no console.
+ * O segundo for é um exemplo de como podemos usar o comando for para contar de 0 a 100 em incrementos de 10, imprimindo cada valor no console.
+ * @author Ranieri Mendes
+ * @version 1.0
+ */
 
 public class RepeticaoFor {
+    public static void main(String[] args) {
+        for(int cc=0; cc<=3; cc++){
+            System.out.println("Cambalhota!");
+        }
+        for(int n=0; n<=100; n+=10){
+            System.out.println(n);
+        }
+    }
 }
--- src/RepeticaoLacosAninhados.java
+++ src/RepeticaoLacosAninhados.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+/** Exemplo simples de estrutura de repetição com laços aninhados, onde temos um loop externo (for) que itera de 1 a 3 e um loop interno (for) que itera de 0 a 2 em incrementos de 2.
+ * @author Ranieri Mendes
+ * @version 1.0
+ */
 
 public class RepeticaoLacosAninhados {
+    public static void main(String[] args) {
+        for(int i=1; i<=3;i++){
+            for(int j=0; j<=2; j+=2){
+                System.out.println(i + "||" + j);
+            }
+        }
+    }
 }
